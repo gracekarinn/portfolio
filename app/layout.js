c5e59322dbfd9da2011900c9ec3c925ba33a8b6c
gracekarinn <gracekarina12@gmail.com>
2024-02-24 17:45:08 +0700

    added analytics n speed

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
       </body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
